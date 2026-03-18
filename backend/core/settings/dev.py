51dad08075f5554dab687ff585c4479aed8eeb9c
Curtis Wirtz <supcurtis@gmail.com>
2026-03-18 11:45:20 -0700

    configure settings.py for CSRF, SESSIONS, and CORS

--- backend/core/settings/dev.py
+++ backend/core/settings/dev.py
@@ -33,27 +33,46 @@ DATABASES = {
 #     }
 # }
 
-# Allows request that include HttpOnly cookies, Authorization: Bearer, credentials "include" in fetch
-CORS_ALLOW_CREDENTIALS = True
-# Allows Django to accept cookies + CSRF tokens
-CSRF_TRUSTED_ORIGINS = os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS', 'http://localhost:5173').split(',')
-# Allows JavaScript in the browser make requests to the Django API from another domain/port
-CORS_ALLOWED_ORIGINS = os.environ.get('DJANGO_CORS_ALLOWED_ORIGINS', 'http://localhost:5173').split(',')
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+# https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-# Without these, the Django Admin area will have broken CSS
+# Specifically... without these, the Django Admin area's CSS will be broken
 STATIC_URL = '/static/'
 STATICFILES_DIRS = []
 STATIC_ROOT = None
 
+# User uploaded static files
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
-# # Allow these addresses (Frontend) to talk to this Backend 
-# CORS_ALLOWED_ORIGINS = ['*']
+# Security settings for decoupled session based auth:
+# Flow: Django sends a cookie to the browser. React looks at that cookie, copies the value, and sends it back in the X-CSRFToken header.
 
-# # CRITICAL: Allow the browser to send cookies (we use sessions auth -> requires Session ID)
-# CORS_ALLOWED_CREDENTIALS = True
+# Keep the CSRF logic and Session logic independent
+CSRF_USE_SESSIONS = False
+
+# CORS
+# Allows the browser to send/receive cookies (we need Session ID)
+CORS_ALLOW_CREDENTIALS = True
+# Allows JavaScript in the browser make requests to the Django API from another domain/port
+CORS_ALLOWED_ORIGINS = os.environ.get('DJANGO_CORS_ALLOWED_ORIGINS', 'http://localhost:5173,http://127.0.0.1:5173').split(',')
+
+# CSRF
+# Allows Django to accept cookies + CSRF tokens
+CSRF_TRUSTED_ORIGINS = os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS', 'http://localhost:5173,http://127.0.0.1:5173').split(',')
+# This allows React to read the CSRF cookie so it can put it in the X-CSRFToken header
+CSRF_COOKIE_HTTPONLY = False
+# Ensure that the browser actually sends the cookie back to the API port
+CSRF_COOKIE_SAMESITE = 'Lax'
+# HTTPS only? True in production
+CSRF_COOKIE_SECURE = not DEBUG
+
+# Sessions
+# Ensure that the browser actually sends the cookie back to the API port
+SESSION_COOKIE_SAMESITE = 'Lax'
+# Blocks JavaScript - never touches session ID
+SESSION_COOKIE_HTTPONLY = True
+# HTTPS only? True in production
+SESSION_COOKIE_SECURE = not DEBUG
--- backend/core/settings/prod.py
+++ backend/core/settings/prod.py
@@ -1,6 +1,8 @@
 from .base import *
 import os
 
+# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower() in ("1", "true", "yes") # DEBUG is always False in production
@@ -8,7 +10,7 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower() in ("1", "true", "yes") 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
 
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -23,18 +25,43 @@ DATABASES = {
     }
 }
 
-# Allows request that include HttpOnly cookies, Authorization: Bearer, credentials "include" in fetch
-CORS_ALLOW_CREDENTIALS = True
-# Allows Django to accept cookies + CSRF tokens
-CSRF_TRUSTED_ORIGINS = os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS').split(',')
-# Allows JavaScript in the browser make requests to the Django API from another domain/port
-CORS_ALLOWED_ORIGINS = os.environ.get('DJANGO_CORS_ALLOWED_ORIGINS').split(',')
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+# https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+# Security settings for decoupled session based auth:
+# Flow: Django sends a cookie to the browser. React looks at that cookie, copies the value, and sends it back in the X-CSRFToken header.
+
+# Keep the CSRF logic and Session logic independent
+CSRF_USE_SESSIONS = False
+
+# CORS
+# Allows the browser to send/receive cookies (we need Session ID)
+CORS_ALLOW_CREDENTIALS = True
+# Allows JavaScript in the browser make requests to the Django API from another domain/port
+CORS_ALLOWED_ORIGINS = os.environ.get('DJANGO_CORS_ALLOWED_ORIGINS').split(',')
+
+# CSRF
+# Allows Django to accept cookies + CSRF tokens
+CSRF_TRUSTED_ORIGINS = os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS').split(',')
+# This allows React to read the CSRF cookie so it can put it in the X-CSRFToken header
+CSRF_COOKIE_HTTPONLY = False
+# Ensure that the browser actually sends the cookie back to the API port
+CSRF_COOKIE_SAMESITE = 'Lax'
+# HTTPS only
+CSRF_COOKIE_SECURE = True
+
+# Sessions
+# Ensure that the browser actually sends the cookie back to the API port
+SESSION_COOKIE_SAMESITE = 'Lax'
+# Blocks JavaScript - never touches session ID
+SESSION_COOKIE_HTTPONLY = True
+# HTTPS only
+SESSION_COOKIE_SECURE = True
