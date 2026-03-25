d0bfe2cbf80c4cfae619faf116329ecb4e7aad9c
Curtis Wirtz <supcurtis@gmail.com>
2026-03-24 22:11:32 -0700

    add DRF view function for login

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework import status
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    @method_decorator(ensure_csrf_cookie)
+    def post(self, request):
+
+        # DRF puts parsed JSON data in request.data
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        # Input fallbacks
+        if not email:
+            return Response({"message": "Email required."}, status=status.HTTP_400_BAD_REQUEST)
+        if not password:
+            return Response({"message": "Password required."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # Check the user credentials
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            # Start the session and send the sessionid cookie
+            login(request, user)
+
+            # Send back the user info in the response body so the frontend can store it in state and display it
+            return Response({
+                "message": "Successfully logged in.",
+                "user": {
+                    "email": user.email,
+                }
+            }, status=status.HTTP_200_OK)
+
+        # Fail gracefully
+        return Response({"message": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
