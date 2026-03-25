5224d007b9c1867916e4bc9ecbb395c1c372460c
Curtis Wirtz <supcurtis@gmail.com>
2026-03-25 07:55:26 -0700

    frontend-feat: install axios and configure client.ts

---
+++ frontend/src/api/client.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true, // Include cookies for auth (must be set to true)
+    xsrfCookieName: "csrftoken",
+    xsrfHeaderName: "X-CSRFToken",
+})
+
+export default api;
