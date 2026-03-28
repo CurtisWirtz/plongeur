27ba2642d6c7df503ff8ea9794bcd9b204c1a0bd
Curtis Wirtz <supcurtis@gmail.com>
2026-03-27 17:30:13 -0700

    include commentary link to discussion on axios csrf issue

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -5,7 +5,7 @@ const api = axios.create({
     withCredentials: true, // Include cookies for auth (must be set to true)
     xsrfCookieName: "csrftoken",
     xsrfHeaderName: "X-CSRFToken",
-    withXSRFToken: true, // Ensure CSRF token is included in requests
+    withXSRFToken: true, // Ensure CSRF token is included in requests https://stackoverflow.com/questions/69002516/axios-not-sending-xsrf-token-with-headers
 })
 
 export default api;
