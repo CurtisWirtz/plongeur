16ce82aaeac7a7099ff62bc96ac3504c5a10f9ec
Curtis Wirtz <supcurtis@gmail.com>
2026-03-31 10:27:15 -0700

    add register zod schema

--- frontend/src/schemas/auth.ts
+++ frontend/src/schemas/auth.ts
@@ -6,3 +6,14 @@ export const loginSchema = z.object({
 });
 
 export type LoginSchemaType = z.infer<typeof loginSchema>;
+
+export const registerSchema = z.object({
+    email: z.email().min(5, "Email must be at least 5 characters").max(150, "Email must be less than 150 characters"),
+    password: z.string().min(3, "Password must be at least 3 characters"),
+    password2: z.string().min(3, "Confirm password must be at least 3 characters"),
+}).refine((data) => data.password === data.password2, {
+  message: "Passwords do not match",
+  path: ["password2"], // this path will attach the error to the password2 field
+});
+
+export type RegisterSchemaType = z.infer<typeof registerSchema>;
