0c0c6681283345657ce40d027c4664c40d794141
Curtis Wirtz <supcurtis@gmail.com>
2026-04-01 03:16:59 -0700

    register form implemented on frontend

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -5,6 +5,7 @@ import { registerSchema } from '@/schemas/auth'
 import type { RegisterSchemaType } from '@/schemas/auth'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
+import { AxiosError } from 'axios'
 
 const useRegister = () => {
     const navigate = useNavigate()
@@ -18,9 +19,9 @@ const useRegister = () => {
             console.log("Registration successful:", data);
             navigate({to: "/"})
         },
-        onError: (error) => {
-            console.error("Registration failed:", error);
-        }
+        onError: (error: AxiosError<{ email: string }>) => {
+            console.error("Registration failed:", error.response?.data);
+        },
     })
 }
 
@@ -29,6 +30,7 @@ const Register = () => {
 
     const { register, handleSubmit, formState: { errors }, } = useForm<RegisterSchemaType>({
         resolver: zodResolver(registerSchema),
+        shouldFocusError: true, // a11y, focus errors when they occur
     })
 
     const onSubmit = (data: RegisterSchemaType) => {
@@ -36,41 +38,65 @@ const Register = () => {
         mutate(data)
     }
 
+    function capitalized(str: string): string {
+        // get the first character and capitalize it
+        const firstChar: string = str[0][0].toUpperCase()
+        // concatenate the capitalized first character with the rest of the string
+        return firstChar + str[0].slice(1);
+    }
+
     return (
         <div style={{ border: '1px solid #ccc', padding: '1rem', maxWidth: '300px' }}>
-        <h3>Please Register</h3>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-            <input 
-                {...register('email')} 
-                type="email" 
-                placeholder="Email" 
-            />
-            {errors.email && (
-                <p style={{ color: 'orange', fontSize: '0.8rem' }}>{errors.email.message}</p>
-            )}
-            </div>
+            <h3>Sign up!</h3>
+            <form onSubmit={handleSubmit(onSubmit)}>
 
-            <br />
-            
-            <div>
-            <input 
-                {...register('password')} 
-                type="password" 
-                placeholder="Password" 
-            />
-            {errors.password && (
-                <p style={{ color: 'orange', fontSize: '0.8rem' }}>{errors.password.message}</p>
-            )}
-            </div>
+                {error?.response?.data?.email && (
+                    <p style={{ color: 'red', fontSize: '0.8rem' }}>{capitalized(error.response.data?.email)}</p>
+                )}
 
-            <br />
+                <div>
+                    <input 
+                        {...register('email')} 
+                        type="email" 
+                        placeholder="Email" 
+                    />
+                    {errors.email && (
+                        <p style={{ color: 'orange', fontSize: '0.8rem' }}>{errors.email.message}</p>
+                    )}
+                </div>
 
-            <button type="submit" disabled={isPending}>
-            {isPending ? 'Logging in...' : 'Login'}
-            </button>
-        </form>
-        {error && <p style={{ color: 'red' }}>Login Failed: {error.message}</p>}
+                <br />
+                
+                <div>
+                    <input 
+                        {...register('password')} 
+                        type="password" 
+                        placeholder="Password" 
+                    />
+                    {errors.password && (
+                        <p style={{ color: 'orange', fontSize: '0.8rem' }}>{errors.password.message}</p>
+                    )}
+                </div>
+
+                <br />
+
+                <div>
+                    <input 
+                        {...register('confirmPassword')} 
+                        type="password" 
+                        placeholder="Confirm Password" 
+                    />
+                    {errors.confirmPassword && (
+                        <p style={{ color: 'orange', fontSize: '0.8rem' }}>{errors.confirmPassword.message}</p>
+                    )}
+                </div>
+
+                <br />
+
+                <button type="submit" disabled={isPending}>
+                    {isPending ? 'Signing up...' : 'Sign up'}
+                </button>
+            </form>
         </div>
     )
 }
--- frontend/src/schemas/auth.ts
+++ frontend/src/schemas/auth.ts
@@ -10,10 +10,10 @@ export type LoginSchemaType = z.infer<typeof loginSchema>;
 export const registerSchema = z.object({
     email: z.email().min(5, "Email must be at least 5 characters").max(150, "Email must be less than 150 characters"),
     password: z.string().min(3, "Password must be at least 3 characters"),
-    password2: z.string().min(3, "Confirm password must be at least 3 characters"),
-}).refine((data) => data.password === data.password2, {
+    confirmPassword: z.string().min(3, "Confirm password must be at least 3 characters"),
+}).refine((data) => data.password === data.confirmPassword, {
   message: "Passwords do not match",
-  path: ["password2"], // this path will attach the error to the password2 field
+  path: ["confirmPassword"], // this path will attach the error to the confirmPassword field
 });
 
 export type RegisterSchemaType = z.infer<typeof registerSchema>;
