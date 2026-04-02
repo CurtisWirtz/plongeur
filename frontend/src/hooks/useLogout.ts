37f5e39573b5476b6da1faf89fc3c8f68fc8fb6b
Curtis Wirtz <supcurtis@gmail.com>
2026-04-02 04:24:17 -0700

    code cleanup: silence noisy logs

--- frontend/src/hooks/useLogout.ts
+++ frontend/src/hooks/useLogout.ts
@@ -10,16 +10,13 @@ const useLogout = () => {
   return useMutation({
     mutationFn: async () => {
       const response = await api.post('/accounts/logout/')
-      console.log("Successfully logged out:", response)
       return response.data
     },
     onSuccess: () => {
-      console.log("success in logout")
       // the Header will update based on the auth-user query, so we need to clear that data out on logout
       queryClient.setQueryData(['auth-user'], null)
       // removes all queries, but isn't as nuclear as .clear() which also resets settings and cache config
-      queryClient.removeQueries( { queryKey: ['auth-user'] })  
-      console.log("aftger invalidating queries")
+      queryClient.removeQueries( { queryKey: ['auth-user'] }) 
 
       navigate({ to: '/login' })
     },
