dbac2656fabf722594548307552bc91ccdf7f8e0
Curtis Wirtz <supcurtis@gmail.com>
2026-04-02 04:03:06 -0700

    resolve eslint errors

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,11 +1,16 @@
 import { Suspense } from 'react'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
+import type { QueryClient } from '@tanstack/react-query'
 import { TanStackRouterDevtools, ReactQueryDevtools } from '@/lib/devTools'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
 
-export const Route = createRootRoute({
+interface MyRouterContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRoute<MyRouterContext>({
   component: () => (
     <>
       <Header />
