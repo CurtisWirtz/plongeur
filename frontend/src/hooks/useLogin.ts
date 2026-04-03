796268fcb1931669298dc4424843d0c691c5bb15
Curtis Wirtz <supcurtis@gmail.com>
2026-04-03 09:56:13 -0700

    use tanstack query's search params to redirect users to intended protected route after logging in

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -1,4 +1,4 @@
-import { useNavigate } from '@tanstack/react-router'
+import { useNavigate, useSearch } from '@tanstack/react-router'
 import api from '@/api/client'
 import type { LoginSchemaType } from '@/schemas/auth'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
@@ -8,6 +8,9 @@ import type { AxiosError } from 'axios'
 const useLogin = () => {
     const navigate = useNavigate()
     const queryClient = useQueryClient()
+
+    // Search params from the current route, if the user tried to access a protected route while not logged in, we'll redirect them there after they successfully login
+    const search = useSearch({ from: '/login' })
 
     return useMutation({
         mutationFn: async (data: LoginSchemaType) => {
@@ -21,7 +24,10 @@ const useLogin = () => {
             // other components looking at auth-user will know the data has changed
             queryClient.invalidateQueries({ queryKey: ['auth-user'] })
             
-            navigate({to: "/"})
+            // If search.redirect exists, use that's where we'll send the user after logging in. Otherwise, redirect them to root
+            const destination = search.redirect || '/dashboard'
+            console.log(`Login success! Redirecting to: ${destination}`)
+            navigate({ to: destination })
         },
         onError: (error: AxiosError<{ detail: string }>) => {
             console.error("Login failed:", error.response?.data?.detail);
--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -3,16 +3,28 @@ import { fetchUser } from '@/api/auth'
 import Login from '@/components/Login'
 import type { QueryClient } from '@tanstack/react-query'
 
+type LoginSearch = {
+  redirect?: string
+}
+
 export const Route = createFileRoute('/login')({
-  beforeLoad: async ({ context }) => {
+  // validate and type the search params, for redirecting users to their intended destination after they log in
+  validateSearch: (search: Record<string, unknown>): LoginSearch => {
+    return {
+      redirect: search.redirect as string | undefined,
+    }
+  },
+  beforeLoad: async ({ context, search }) => {
     try {
       const user = await (context as { queryClient: QueryClient }).queryClient.ensureQueryData({
         queryKey: ['auth-user'],
         queryFn: fetchUser,
       })
       if (user) { 
-        // inside a try/catch block, this throws and error, but we can catch the error type and redirect there!
-        throw redirect({ to: '/' }) 
+        // inside a try/catch block, this throws an error..sending us to 'catch (err)', but we can catch the error type and redirect there!
+        throw redirect({ 
+          to: search.redirect || '/dashboard'
+        })
       }
     } catch (err) {
       // If the error is actually a redirect, re-throw the error!
