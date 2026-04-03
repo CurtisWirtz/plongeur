b8584d08acd085b73513ba2e13f6dd387c63d605
Curtis Wirtz <supcurtis@gmail.com>
2026-04-03 09:54:07 -0700

    add beforeLoad bouncer file for protected routes: _authenticated

--- frontend/src/routes/_authenticated.tsx
+++ frontend/src/routes/_authenticated.tsx
@@ -3,7 +3,7 @@ import type { QueryClient } from '@tanstack/react-query'
 import { fetchUser } from '@/api/auth'
 
 export const Route = createFileRoute('/_authenticated')({
-  beforeLoad: async ({ context }) => {
+  beforeLoad: async ({ context, location }) => {
     try {
       await (context as { queryClient: QueryClient }).queryClient.ensureQueryData({
         queryKey: ['auth-user'],
@@ -11,7 +11,13 @@ export const Route = createFileRoute('/_authenticated')({
       })
     } catch {
       // not authenticated, redirect to login page
-      throw redirect({ to: '/login' })
+      throw redirect({ 
+        to: '/login',
+        search: { // If an unauthorized user tries to access a protected page, we want them to be returned to the original page they wanted to visit after they log in
+            // This puts ?redirect=/dashboard in the URL
+            redirect: location.href
+        } 
+      })
     }
   },
 })
