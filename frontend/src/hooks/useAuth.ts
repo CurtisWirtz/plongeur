0f25864d011c4598a3d6a1f11d62a83aa9206d1b
Curtis Wirtz <supcurtis@gmail.com>
2026-04-05 08:27:36 -0700

    explicity set the auth-user to null in cache if blocked from protected route. prevents duplicate requests for auth

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -8,7 +8,6 @@ export const useAuth = () => {
       queryFn: fetchUser,
       retry: false, 
       staleTime: Infinity, // Keep the user in memory until logged out or the page is refreshed, otherwise we get duplicate requests to the backend for the user, even though the auth data is already in the cache
-      placeholderData: null, // if the user data is wiped null, the hook doesn't accidentally trigger a global (e.g. the header) loading state.
       enabled: false
   })
 }
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -18,9 +18,13 @@ export const Route = createRootRoute<MyRouterContext>({
       return await (context as { queryClient: QueryClient }).queryClient.ensureQueryData({
         queryKey: ['auth-user'],
         queryFn: fetchUser,
-        staleTime: 5 * 60 * 1000, // Keep the user in memory until logged out or the page is refreshed
+        staleTime: Infinity, // Keep the user in memory until logged out or the page is refreshed
       })
     } catch {
+      // if a non-auth user attempts to access a protected page, a request to the backend will fail, then _authenticated will redirect the user to the login page, where they will AGAIN request the backend for user data 
+      // explicitly setting the user to null in cache here will prevent the 2nd requests to the backend for the user, as the login page will see that the user isn't authenticated
+      // at that point, only logging in or registering will update the cache to authorize a user to see the page
+      (context as { queryClient: QueryClient }).queryClient.setQueryData(['auth-user'], null)
       // If unauthorized, the user is null
       return { user: null }
     }
