efd7c2813861e4bed0c1d53de3fb03117ed7b6aa
Curtis Wirtz <supcurtis@gmail.com>
2026-04-06 06:19:55 -0700

    test page fix: remove duplicate user data request by calling useQueryClient over useAuth

--- frontend/src/routes/_authenticated/dashboard/test.tsx
+++ frontend/src/routes/_authenticated/dashboard/test.tsx
@@ -1,13 +1,17 @@
-import { useAuth } from '@/hooks/useAuth'
+import { useQueryClient } from '@tanstack/react-query'
 
 export default function TestComponent() {
-  const user = useAuth()
+  // const user = null //useAuth() 
+
+  const queryClient = useQueryClient()
+  const user: { email: string } | null | undefined = queryClient.getQueryData(['auth-user'])
+
   console.log("user", user)
   
   return (
     <div className="flex flex-col">
       <h1>Hello "/_authenticated/test/"!</h1>
-      {user && <p>Welcome, {user.data?.email}!</p>}
+      {user && <p>Welcome, {user.email}!</p>}
     </div>
   )
 }
