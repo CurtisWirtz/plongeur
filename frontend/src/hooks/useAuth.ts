6ac7aa9ee7142560f789e030208ad938d59877c0
Curtis Wirtz <supcurtis@gmail.com>
2026-04-06 22:53:47 -0700

    useAuth hook enabled set to true.  hook needs to trigger in Header when data is updated

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -7,6 +7,5 @@ export const useAuth = () => {
       queryFn: fetchUser,
       retry: false, 
       staleTime: Infinity, // Keep the user in memory until logged out or the page is refreshed, otherwise we get duplicate requests to the backend for the user, even though the auth data is already in the cache
-      enabled: false
   })
 }
