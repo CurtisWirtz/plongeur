b5569afb593fe0dd8ccac9a78d86cd56c9fb7c29
Curtis Wirtz <supcurtis@gmail.com>
2026-04-08 01:36:52 -0700

    install ShadCN and adding the test a few components. Button, DropDownMenu

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,10 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Link, createFileRoute } from '@tanstack/react-router'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/')({
   component: () => (
     <>
       <h1>Plongeur</h1>
       <p>Welcome to Plongeur! This is the frontend.</p>
+      <Button asChild variant="default" size="lg">
+        <Link to="/dashboard">Go to Dashboard</Link>
+      </Button>
+      
     </>
   ),
 })
