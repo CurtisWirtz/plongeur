cb301a22ae597a9168dd22bdb91f25f138ceb531
Curtis Wirtz <supcurtis@gmail.com>
2026-04-10 02:17:25 -0700

    basic Header dropdown when logged in

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,31 +2,70 @@ import Logout from '@/components/Logout'
 import { Link } from '@tanstack/react-router'
 import { useAuth } from '@/hooks/useAuth'
 import type { User } from '@/types/user.types'
+import { Avatar, AvatarFallback } from "@/components/ui/avatar"
+
+import { UserRound } from 'lucide-react'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from './ui/button'
 
 const Header = () => {
   const { data: user } = useAuth() as { data: User | null }
 
   return (
-    <header style={{ display: 'flex', justifyContent: 'space-between', padding: '1rem', borderBottom: '1px solid #eee' }}>
-      <nav>
-        <Link to="/" className="font-bold">Plongeur</Link>
-      </nav>
-      <div style={{ display: 'flex', gap: '1rem', alignItems: 'center' }}>
+    <header className="w-full sticky top-0 bg-primary-foreground">
+      <nav className="flex justify-between w-full border-b px-4 border-accent pt-2 pb-3">
+        <Link to="/" className="font-bold flex items-center">
+          <h1>Plongeur</h1>
+        </Link>
+      
         {user ? (
-          <>
-            {/* Display the user data from your Django UserSerializer */}
-            <Link to="/dashboard" className="font-bold">
-              <span>Hi, {user.email}</span>
-            </Link>
-            <Logout />
-          </>
+          <div>
+            <DropdownMenu>
+              <DropdownMenuTrigger className="cursor-pointer">
+              <Avatar className="shadow-2xl border" >
+                {/* <AvatarImage /> */}
+                <AvatarFallback>
+                  <UserRound />
+                </AvatarFallback>
+              </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="space-y-2 w-full text-center -translate-x-7">
+                <DropdownMenuLabel >{user.email}</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem asChild >
+                  <Button asChild variant="ghost" className="w-full cursor-pointer">
+                    <Link to="/dashboard">Dashboard</Link>
+                  </Button>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                  <Logout />
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         ) : (
-          <>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-          </>
+          <div>
+            <Button asChild variant="ghost">
+              <Link to="/login" activeProps={{ className: "bg-secondary shadow-sm" }}>
+                Login
+              </Link>
+            </Button>
+            <Button asChild variant="ghost">
+              <Link to="/register" activeProps={{ className: "bg-secondary shadow-sm" }}>
+                Register
+              </Link>
+            </Button>
+          </div>
         )}
-      </div>
+      </nav>
     </header>
   )
 }
