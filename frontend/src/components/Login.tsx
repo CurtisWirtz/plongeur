0223ebe3d6affcc0360dd78b7ff50e9765256fa4
Curtis Wirtz <supcurtis@gmail.com>
2026-04-12 15:06:56 -0700

    apply react-hook-form control and shadcn styling to Login form

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,12 +1,31 @@
 import { loginSchema } from '@/schemas/auth'
 import type { LoginSchemaType } from '@/schemas/auth'
-import { useForm } from 'react-hook-form'
+import { Controller, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import useLogin from '@/hooks/useLogin'
+import { Button } from '@/components/ui/button'
+import { Link } from '@tanstack/react-router'
 
+import { 
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardAction,
+  CardContent,
+  CardFooter,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { 
+  Field,
+  FieldLabel,
+  FieldError,
+  FieldGroup
+} from '@/components/ui/field'
+import { Spinner } from "@/components/ui/spinner"
 
 const Login = () => {
-  const { register, handleSubmit, formState: { errors }, } = useForm<LoginSchemaType>({
+  const form = useForm<LoginSchemaType>({
     resolver: zodResolver(loginSchema),
     shouldFocusError: true, // a11y, focus errors when they occur
   })
@@ -19,45 +38,78 @@ const Login = () => {
   }
 
   return (
-    <div style={{ border: '1px solid #ccc', padding: '1rem', maxWidth: '300px' }}>
-      <h3>Please Log In</h3>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-
-          {error?.response?.data?.detail && (
-            <p style={{ color: 'red', fontSize: '0.8rem' }}>{error.response.data?.detail}</p>
-          )}
-
-          <input 
-            {...register('email')} 
-            type="email" 
-            placeholder="Email" 
-          />
-          {errors.email && (
-            <p style={{ color: 'orange', fontSize: '0.8rem' }}>{errors.email.message}</p>
-          )}
-        </div>
-
-        <br />
-        
-        <div>
-          <input 
-            {...register('password')} 
-            type="password" 
-            placeholder="Password" 
-          />
-          {errors.password && (
-            <p style={{ color: 'orange', fontSize: '0.8rem' }}>{errors.password.message}</p>
-          )}
-        </div>
-
-        <br />
-
-        <button type="submit" disabled={isPending}>
-          {isPending ? 'Logging in...' : 'Login'}
-        </button>
-      </form>
-    </div>
+    <section className="container mt-7 md:mt-14">
+      <Card className="max-w-100 mx-auto">
+        <CardHeader>
+          <CardTitle>Login to your account:</CardTitle>
+          <CardDescription>
+            Enter your credentials
+          </CardDescription>
+          <CardAction className="flex flex-col text-center">
+            <span className='mb-1 text-sm'>New here?</span>
+            <Button variant="outline" className="cursor-pointer shadow-2xl" asChild>
+              <Link to="/register">Register</Link>
+            </Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+          <form id="login" onSubmit={form.handleSubmit(onSubmit)}>
+            <FieldGroup>
+              <Controller 
+                name="email"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="email">E-Mail</FieldLabel>
+                    <Input 
+                      {...field} 
+                      id="email"
+                      aria-invalid={fieldState.invalid}
+                      type="email" 
+                      placeholder="Email"
+                      required
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller 
+                name="password"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="password">Password</FieldLabel>
+                    <Input 
+                      {...field} 
+                      id="password"
+                      aria-invalid={fieldState.invalid}
+                      type="password" 
+                      placeholder="Password" 
+                      required
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+            </FieldGroup>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <Field>
+            <Button form="login" type="submit" className="shadow-2xl" disabled={isPending}>
+              {isPending ? <Spinner /> : 'Login'}
+            </Button>
+            {error?.response?.data?.detail && (
+              <FieldError className="text-red-500 text-center mt-2">{error.response.data?.detail}</FieldError>
+            )}
+          </Field>
+        </CardFooter>
+      </Card>
+    </section>
   )
 }
 
