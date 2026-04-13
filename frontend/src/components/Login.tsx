41b0a1382cecfdd79eff6862b2a78ebc3f74ff13
Curtis Wirtz <supcurtis@gmail.com>
2026-04-13 15:59:36 -0700

    moved login and logout hooks back to the component. better readability

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -2,10 +2,14 @@ import { loginSchema } from '@/schemas/auth'
 import type { LoginSchemaType } from '@/schemas/auth'
 import { Controller, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import useLogin from '@/hooks/useLogin'
-import { Button } from '@/components/ui/button'
+import { Spinner } from "@/components/ui/spinner"
+import { useNavigate, useSearch } from '@tanstack/react-router'
+import api from '@/api/client'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import type { AxiosError } from 'axios'
 import { Link } from '@tanstack/react-router'
 
+import { Button } from '@/components/ui/button'
 import { 
   Card,
   CardHeader,
@@ -22,7 +26,35 @@ import {
   FieldError,
   FieldGroup
 } from '@/components/ui/field'
-import { Spinner } from "@/components/ui/spinner"
+
+
+const useLogin = () => {
+    const navigate = useNavigate()
+    const queryClient = useQueryClient()
+
+    // Search params from the current route, if the user tried to access a protected route while not logged in, we'll redirect them there after they successfully login
+    const search = useSearch({ from: '/login' })
+
+    return useMutation({
+        mutationFn: async (data: LoginSchemaType) => {
+            const response = await api.post('/accounts/login/', data)
+            return response.data
+        },
+        onSuccess: (data) => {
+            console.log("Login successful:", data);
+            // Update the auth-user query with the logged-in User object/data
+            queryClient.setQueryData(['auth-user'], data.user) 
+            
+            // If search.redirect exists, use that's where we'll send the user after logging in. Otherwise, redirect them to root
+            const destination = search.redirect || '/dashboard'
+            console.log(`Login success! Redirecting to: ${destination}`)
+            navigate({ to: destination })
+        },
+        onError: (error: AxiosError<{ detail: string }>) => {
+            console.error("Login failed:", error.response?.data?.detail);
+        },
+    })
+}
 
 const Login = () => {
   const form = useForm<LoginSchemaType>({
