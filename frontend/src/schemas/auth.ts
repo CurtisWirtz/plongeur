1f7212a866cda47364cbc781f7c2b88604ecd04e
Curtis Wirtz <supcurtis@gmail.com>
2026-04-30 01:20:47 -0700

    added zod schema for finalize registration

--- frontend/src/schemas/auth.ts
+++ frontend/src/schemas/auth.ts
@@ -35,3 +35,17 @@ export const registerSchema = z.object({
   path: ["confirm_password"], // this path will attach the error to the confirmPassword field
 });
 export type RegisterSchemaType = z.infer<typeof registerSchema>;
+
+export const finalizeRegistrationSchema = z.object({
+    email: z.email().min(5, "Email must be at least 5 characters").max(150, "Email must be less than 150 characters"),
+    password: z.string().min(3, "Password must be at least 3 characters"),
+    confirm_password: z.string().min(3, "Password must be at least 3 characters"),
+    // Honeypot trap #1: Simple hidden input that must be blank
+    website: z.string().max(0).optional(),
+    // Honeypot trap #2: This value must match the "captcha" value generated on the backend and injected via Javascript
+    confirm_email: z.string().min(1, "Verification failed."),
+}).refine((data) => data.password === data.confirm_password, {
+  message: "Passwords do not match",
+  path: ["confirm_password"], // this path will attach the error to the confirmPassword field
+});
+export type FinalizeRegistrationSchemaType = z.infer<typeof finalizeRegistrationSchema>;
