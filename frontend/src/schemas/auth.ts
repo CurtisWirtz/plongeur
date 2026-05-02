46612e0c5e1aa56cdc5faa737b3ffbc90439a886
Curtis Wirtz <supcurtis@gmail.com>
2026-05-01 21:11:36 -0700

    finalize zod schema will allow for blank (empty) optional responses

--- frontend/src/schemas/auth.ts
+++ frontend/src/schemas/auth.ts
@@ -24,31 +24,19 @@ export const verifyRegistrationSchema = z.object({
 });
 export type VerifyRegistrationSchemaType = z.infer<typeof verifyRegistrationSchema>;
 
-export const registerSchema = z.object({
-    email: z.email().min(5, "Email must be at least 5 characters").max(150, "Email must be less than 150 characters"),
-    password: z.string().min(3, "Password must be at least 3 characters"),
-    confirm_password: z.string().min(3, "Password must be at least 3 characters"),
-    // Honeypot trap #1: Simple hidden input that must be blank
-    website: z.string().max(0).optional(),
-    // Honeypot trap #2: This value must match the "captcha" value generated on the backend and injected via Javascript
-    confirm_email: z.string().min(1, "Verification failed."),
-}).refine((data) => data.password === data.confirm_password, {
-  message: "Passwords do not match",
-  path: ["confirm_password"], // this path will attach the error to the confirmPassword field
-});
-export type RegisterSchemaType = z.infer<typeof registerSchema>;
-
 export const finalizeRegistrationSchema = z.object({
-    email: z.email().min(5, "Email must be at least 5 characters").max(150, "Email must be less than 150 characters"),
-    password: z.string().min(3, "Password must be at least 3 characters"),
-    confirm_password: z.string().min(3, "Password must be at least 3 characters"),
+  email: z.email().min(5, "Email must be at least 5 characters").max(150, "Email must be less than 150 characters"),
+  password: z.string().min(3, "Password must be at least 3 characters"),
+  confirm_password: z.string().min(3, "Password must be at least 3 characters"),
   first_name: z.string()
     .max(150, "First name cannot exceed 150 characters.")
     .regex(nameRegex, "Name contains invalid characters")
+    .or(z.literal(""))
     .optional(),
   last_name: z.string()
     .max(150, "First name cannot exceed 150 characters.")
     .regex(nameRegex, "Name contains invalid characters")
+    .or(z.literal(""))
     .optional(),
   phone_number: z.string().max(150, "First name cannot exceed 150 characters.").optional(),
 }).refine((data) => data.password === data.confirm_password, {
